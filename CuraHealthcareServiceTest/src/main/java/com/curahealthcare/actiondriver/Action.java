f1118c9c9cc82f4fdaf2bf4e6abc04d38099627d
Pooja Gosavi <poojagosavi2526@gmail.com>
2025-03-05 19:23:56 +0530

    2nd commit

--- CuraHealthcareServiceTest/src/main/java/com/curahealthcare/actiondriver/Action.java
+++ CuraHealthcareServiceTest/src/main/java/com/curahealthcare/actiondriver/Action.java
@@ -24,10 +24,6 @@ package com.curahealthcare.actiondriver;
 	import org.openqa.selenium.support.ui.WebDriverWait;
 	import com.curahealthcare.base.BaseClass;
 
-	/**
-	 * @author Hitendra Verma added on 13th March2019
-	 *
-	 */
 	public class Action extends BaseClass {
 
 		public static WebDriver driver;
